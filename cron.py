613b9785a4d1ff45a996d4b5959207afda58640b
Ubuntu <crawler@crawler.4g4elbp5cpmebbw0ofagxo1nlf.dx.internal.cloudapp.net>
2019-08-02 14:06:14 +0000

    Data files

--- cron.py
+++ cron.py
@@ -10,6 +10,9 @@ job.setall('0 6 * * *')
 job2 = cron.new(command='python3 /home/crawler/appexchange/crawing/spider_microsoft.py')
 job2.setall('0 12 * * *')
 
+job3 = cron.new(command='python3 /home/crawler/appexchange/reviews/salesforce.py')
+job3.setall('0 9 * * 3')
+
 for entry in cron:
     print(entry)
 cron.write()
